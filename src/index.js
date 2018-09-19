6214c56845d8af9c13bf8e1477657297ddddc3bd
globalavocado <globalavocado@users.noreply.github.com>
2018-09-19 11:48:57 +0100

    base map with central marker, amended README

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Map,
+  TileLayer,
+  Marker
+} from 'react-leaflet';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class AppiaAntica extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 41.81,
+      lng: 12.56,
+      zoom: 12,
+    };
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      // <div className="App">
+      //   <h1 className="App-title">Appia Antica</h1>
+          <Map center={position} zoom={this.state.zoom}>
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png" />
+            <Marker position={[this.state.lat, this.state.lng]} />
+          </Map>
+      // </div>
+    );
+  }
+}
+
+ReactDOM.render(<AppiaAntica />, document.getElementById('app'));
 registerServiceWorker();
